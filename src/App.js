5b7d9e558c6c76f5a689f5476a0b8ef7b3d12db0
ahmedaljabbari <ahmedaljabbari@outlook.com>
2021-01-05 09:14:27 +0100

    Function for Filtering the list

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import './App.css';
-import SearchIcon from "../src/Images/search.png"
-import DREAMS from "./dreams-data.json"
+/** @format */
+
+import "./App.css";
+import SearchIcon from "../src/Images/search.png";
+import DREAMS from "./dreams-data.json";
+import { useState } from "react";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <div className="App">
-        <div className="search">
-          <input type="text" className="searchTerm" placeholder="What are you looking for?"></input>
-          <button type="submit" className="searchButton"><img src={SearchIcon}></img> </button>
-        </div>
+      <div className="search">
+        <input
+          type="text"
+          className="searchTerm"
+          placeholder="What are you looking for?"
+          onChange={(event) => {
+            setSearchTerm(event.target.value);
+          }}
+        ></input>
+        <button type="submit" className="searchButton">
+          <img src={SearchIcon}></img>{" "}
+        </button>
+      </div>
 
-        <section>
-          {DREAMS.map((dream, index) => {
-            return <div>
+      <section>
+        {DREAMS.filter((val) => {
+          if (searchTerm === "") {
+            return val;
+          } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+            return val;
+          }
+        }).map((dream, index) => {
+          return (
+            <div key={index}>
               <h4>{dream.title}</h4>
             </div>
-          })}
-        </section>
+          );
+        })}
+      </section>
     </div>
   );
 }
