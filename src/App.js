ddf0a75187301c2f2afb83b545a652af50298926
ahmedaljabbari <ahmedaljabbari@outlook.com>
2021-01-06 04:45:51 +0100

    Converted  the mainfunctional commponent to class component to use the power of 'state'

--- src/App.js
+++ src/App.js
@@ -1,46 +1,58 @@
 /** @format */
 
+import React from "react";
 import "./App.css";
 import SearchIcon from "../src/Images/search.png";
 import DREAMS from "./dreams-data.json";
 import { useState } from "react";
 
-function App() {
-  const [searchTerm, setSearchTerm] = useState("");
+class App extends React.Component {
+  state = {
+    searchValue: "",
+  };
 
-  return (
-    <div className="App">
-      <div className="search">
-        <input
-          type="text"
-          className="searchTerm"
-          placeholder="What are you looking for?"
-          onChange={(event) => {
-            setSearchTerm(event.target.value);
-          }}
-        ></input>
-        <button type="submit" className="searchButton">
-          <img src={SearchIcon}></img>{" "}
-        </button>
+  setSearchValue = (event) => {
+    this.setState({
+      searchValue: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="search">
+          <input
+            type="text"
+            className="searchTerm"
+            placeholder="What are you looking for?"
+            onChange={this.setSearchValue}
+          ></input>
+          <button type="submit" className="searchButton">
+            <img src={SearchIcon}></img>{" "}
+          </button>
+        </div>
+
+        <section>
+          {DREAMS.map((dream, index) => {
+            if (
+              this.state.searchValue !== "" &&
+              dream.title
+                .toLowerCase()
+                .indexOf(this.state.searchValue.toLowerCase()) === -1
+            ) {
+              return null;
+            }
+            return (
+              <div key={index}>
+                <h4>{dream.title}</h4>
+                <p>{dream.content}</p>
+              </div>
+            );
+          })}
+        </section>
       </div>
-
-      <section>
-        {DREAMS.filter((val) => {
-          if (searchTerm === "") {
-            return val;
-          } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())) {
-            return val;
-          }
-        }).map((dream, index) => {
-          return (
-            <div key={index}>
-              <h4>{dream.title}</h4>
-            </div>
-          );
-        })}
-      </section>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
