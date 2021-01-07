c39069dc6428f7d170e1902008dc8db7d4b421df
ahmedaljabbari <ahmedaljabbari@outlook.com>
2021-01-07 07:48:42 +0100

    Delete function fixed && simple edit function created

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import SearchIcon from "../src/Images/search.png";
 import DREAMS from "./dreams-data.json";
 import List from "./components/list";
 import Radio from "./components/RadioButtons";
-import {library} from "@fortawesome/fontawesome-svg-core";
-import {faTrash} from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
 
 library.add(faTrash);
@@ -19,9 +19,11 @@ class App extends React.Component {
       searchValue: "",
       rubrik: "",
       text: "",
+      key:"",
 
-      newGenre: ""
+      newGenre: "",
     };
+    this.setUpdate = this.setUpdate.bind(this);
   }
 
   handleInput = (event) => {
@@ -48,13 +50,13 @@ class App extends React.Component {
     });
   };
 
-  
   addItem = (e) => {
     e.preventDefault();
     const newItem = {
       title: this.state.rubrik,
       content: this.state.text,
-      Genre: this.state.newGenre
+      Genre: this.state.newGenre,
+      key: Date.now()
     };
     console.log(newItem);
     if (newItem.rubrik !== "") {
@@ -62,19 +64,31 @@ class App extends React.Component {
       this.setState({
         items: newItems, // خلينه اللسته الجديده بعد اضافة العنصر بمكان اللسته الاصليه
         rubrik: "",
-        text: "",         //فرغنا الحقول بعد اظافة العنصر الجديد
-        newGenre:""
+        text: "", //فرغنا الحقول بعد اظافة العنصر الجديد
+        newGenre: "",
+        key:""
       });
     }
   };
 
-  deleteItem = (index) => {
-    const filteredItems = [...this.state.items];
-    filteredItems.splice(index, 1);
-      this.setState({
-        items:filteredItems
-      })
-  }
+  deleteItem = (key) => {
+    const filteredItems = this.state.items.filter(dream => dream.key !== key);
+    this.setState({
+      items: filteredItems,
+    });
+  };
+
+  setUpdate(title, key){
+    const editedList = this.state.items;
+    editedList.map((dream) => {
+      if(dream.key === key){
+        dream.title = title
+      }
+    })
+    this.setState({
+      items:editedList
+    })
+  };
 
   render() {
     return (
@@ -109,19 +123,22 @@ class App extends React.Component {
                 value={this.state.text}
                 placeholder="Describe your dream ..."
               ></textarea>{" "}
-                      
-              <Radio newGenre={this.state.newGenre} changeRadio={this.changeRadio} />
-
+              <Radio
+                newGenre={this.state.newGenre}
+                changeRadio={this.changeRadio}
+              />
               <button type="submit">Add</button>
             </form>
           </div>
-          <div id="slider">
-            + Add your dream
-          </div>
-
+          <div id="slider">+ Add your dream</div>
         </section>
 
-        <List listItems={this.state.items} searchValue={this.state.searchValue} deleteDream={this.deleteItem} />
+        <List
+          listItems={this.state.items}
+          searchValue={this.state.searchValue}
+          deleteItem={this.deleteItem}
+          setUpdate={this.setUpdate}
+        />
       </div>
     );
   }
--- src/components/list.js
+++ src/components/list.js
@@ -1,37 +1,43 @@
 import React from "react";
-import "../assets/list.css"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import "../assets/list.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function List(props){
+function List(props) {
   const items = props.listItems;
   const searchValue = props.searchValue;
-  const deleteDream = props.deleteDream; 
+ // const deleteDream = props.deleteDream(dream.key);
   return (
     <div>
       <section>
-          {items.map((dream, index) => {
-            if (
-              searchValue !== "" &&
-              dream.title
-                .toLowerCase()
-                .indexOf(searchValue.toLowerCase()) === -1
-            ) {
-              return null;
-            }
-            return (
-              <div id="dream-box" key={dream.index}>
-                <h3>{dream.title}</h3>
-                <p>{dream.content}</p>
-                <p id="typ">{dream.Genre}</p>
-                <span onClick={deleteDream}>
-                  <FontAwesomeIcon className="faicon" icon="trash" />
-                </span>
-              </div>
-            );
-          })}
-        </section>
+        {items.map((dream) => {
+          if (
+            searchValue !== "" &&
+            dream.title.toLowerCase().indexOf(searchValue.toLowerCase()) === -1
+          ) {
+            return null;
+          }
+          return (
+            <div id="dream-box" key={dream.key}>
+              <input
+                type="text"
+                className="title-box"
+                id={dream.key}
+                value={dream.title}
+                onChange={(e) => {
+                  props.setUpdate(e.target.value, dream.key);
+                }}
+              ></input>
+              <p>{dream.content}</p>
+              <p id="typ">{dream.Genre}</p>
+              <span onClick={() => props.deleteItem(dream.key)}>
+                <FontAwesomeIcon className="faicon" icon="trash" />
+              </span>
+            </div>
+          );
+        })}
+      </section>
     </div>
-  )
+  );
 }
 
-export default List
+export default List;
